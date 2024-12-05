7aca10903fbaf158b4c095b846ce0c0c39c2d187
Mount Tai <3162853966@qq.com>
2024-12-05 22:44:44 +0800

    del: error conf

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -65,24 +65,7 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          }
-        ]
-      }      
+      }    
     ]
   }
 }
